28fdd6e7726f9b80384e66d3fd7faf9436d9e67b
Anton <AGlomadov@stud.kantiana.ru>
2021-09-29 16:17:38 +0200

    lab3

--- app/src/main/java/ru/glomadov/univerproject/MainActivity.kt
+++ app/src/main/java/ru/glomadov/univerproject/MainActivity.kt
@@ -3,11 +3,6 @@ package ru.glomadov.univerproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.glomadov.univerproject.databinding.ActivityMainBinding
 import ru.glomadov.univerproject.profile.ProfileActivity
 
@@ -23,15 +18,5 @@ class MainActivity : AppCompatActivity() {
         binding.profile.setOnClickListener{
             startActivity(Intent(this, ProfileActivity::class.java))
         }
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
